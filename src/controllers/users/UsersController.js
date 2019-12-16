8ef22272aaff3f6546ee65c47c7cc1f85bbdcce2
Markus Tarn <Markus.Tarn@myjar.com>
2019-12-16 09:52:08 +0100

    Added gift recever assigner

--- src/controllers/users/UsersController.js
+++ src/controllers/users/UsersController.js
@@ -11,8 +11,29 @@ module.exports = {
   },
 
   assign: async (req, res) => {
-    const participants = await User.findOneAndUpdate({ name: 'Markus Tarn' }, { receiver: 'Test' })
-    res.json(participants)
+    let solved = true
+    const participants = await User.find({})
+    const indexList = []
+    do {
+      const partyList = participants.map(p => p.name)
+      indexList.length = 0
+      participants.forEach(papa => {
+        let index = Math.floor(Math.random() * partyList.length)
+        while (partyList[index] === papa.name && partyList.length > 1) {
+          index = Math.floor(Math.random() * partyList.length)
+        }
+        indexList.push(partyList.splice(index, 1)[0])
+      })
+      for (let i = 0; i < indexList.length; i++) {
+        if (indexList.length !== participants.length && indexList[i] === participants[i].name) {
+          solved = false
+        }
+      }
+    } while (!solved)
+    for (let u = 0; u < participants.length; u++) {
+      await User.findOneAndUpdate({ name: participants[u].name }, { receiver: indexList[u] })
+    }
+    res.json({ message: 'ok' })
   },
 
   logout: async (req, res) => {
